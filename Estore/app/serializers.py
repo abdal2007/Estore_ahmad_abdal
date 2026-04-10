dd286f2b0b02cbd48c22843be50b0c92f71ddcee
abdal2007 <abdalmaster4@gmail.com>
2026-04-10 15:04:26 +0500

    serializer validation added

--- Estore/app/serializers.py
+++ Estore/app/serializers.py
@@ -5,3 +5,23 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model=Product
         fields='__all__'
+
+    #price validator
+    def validate_price(self,value):
+        if value<0:
+            raise serializers.ValidationError("Product price must be positive")
+        return value
+    # title validator
+    def validate_title(self, value):
+        if len(value)<3:
+            raise serializers.ValidationError("Title should be more than 3 characters long")
+        return value
+    
+    #description validator
+    def validate_description(self, value):
+        if len(value)<10:
+            raise serializers.ValidationError("Product Description should be more than 10 characters long! ")
+        return value
+    
+
+    
